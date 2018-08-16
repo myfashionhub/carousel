c24a57a7c56919abdfa8fd9855058c43d146130b
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2018-08-16 15:32:03 -0400

    Working script

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ var productImages = [];
 for (var i = 1; i < 7; i++) {
 	productImages.push('assets/' + i + '.jpeg');
 }
-// image width 140px - 160px
-
 
 function Carousel() {
+	var self = this;
+
 	this.run = function() {
 		this.imageWidth = 140;
+		this.numSlides = 4; // Number of slides to show at once
 		this.arrows = document.getElementsByClassName('arrow');
-		this.bambox = document.querySelector('#bambox');
 		this.slideCont = document.querySelector('.slide-container');
+
 		this.setUpSlides();
+
+		for (var i = 0; i < this.arrows.length; i++) {
+			this.arrows[i].addEventListener('click', function(e) {
+				self.onArrowClick(e.target.parentNode);
+			});
+		}
 	};
 
 	this.setUpSlides = function() {
@@ -22,35 +29,19 @@ function Carousel() {
 			slide.style.backgroundImage = 'url('+productImages[i]+')';
 			this.slideCont.appendChild(slide);
 		}
-		this.onClick();
 	};
 
-	this.onClick = function () {
-		for (var i = 0; i < this.arrows.length; i++) {
-			this.arrows[i].addEventListener('click', this.onArrowClick.bind(this));
-		}
-	}
-
 	this.onArrowClick = function(element) {
-		console.log(element);
-		// bind click event to arrow left and right
-		// shift content margin-left or right 4x image width
 		var slides = document.getElementsByClassName('slide');
 		var firstSlide = slides[0];
-		// set shift for 4 items
-		var shiftLength = -4 * this.imageWidth;
+		var shiftLength = -this.numSlides * this.imageWidth;
 
-		// sett up conditional for left or right arrow click
-		for(var i = 0; i < this.arrows.length; i++) {
-			var arrow = this.arrows[i];
-			// checking classname for instance of left or right
-			console.dir(arrow);
-			if (arrow.className.indexOf('left') !== -1) {
-				firstSlide.style.marginLeft = 0 + 'px';
-			} else {
-				// setting first slide margin 
-				firstSlide.style.marginLeft = shiftLength + 'px'
-			}
+		var direction = element.className.replace('arrow ', '');
+
+		if (direction === 'left') {
+			firstSlide.style.marginLeft = 0 + 'px';
+		} else {
+			firstSlide.style.marginLeft = shiftLength + 'px';
 		}
 	};
 }
@@ -60,6 +51,7 @@ var checkDocReady = setInterval(function() {
 	if (document.readyState === 'complete') {
 		clearInterval(checkDocReady);
 	}
+
 	var carousel = new Carousel();
 	carousel.run();
 }, 500);
